624f1e43cac81576270a27255e71ee38365344b2
AndreasM <AndreasM@TCFILMD50507780.ffe.foxeg.com>
2015-03-11 09:36:11 -0700

    Organized import

--- src/com/fox/it/erws/rest/api/pojos/ConsumingApplicationPOJO.java
+++ src/com/fox/it/erws/rest/api/pojos/ConsumingApplicationPOJO.java
@@ -18,7 +18,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
 
 
 
